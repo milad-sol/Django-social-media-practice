cec5a3ea013e6e7f3c553f9cd6366b9c550342d9
milad-sol <lvlilad_soltani@yahoo.com>
2024-11-03 20:11:17 +0330

    add register form with clean validations methods

---
+++ account/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegistrationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+class RegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = "account/register.html"
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            User.objects.create_user(
+                username=data["username"],
+                email=data["email"],
+                password=data["password"],
+            )
+            messages.success(request, "Account created successfully", "success")
+            return redirect("home:home")
+
+        return render(request, self.template_name, {"form": form})
