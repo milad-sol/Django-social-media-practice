aea96032eb99fec8293f01be3ff92073939c0c03
milad-sol <lvlilad_soltani@yahoo.com>
2024-11-03 20:39:46 +0330

    overwrite clean method for form validations . reson:add password and repeat password validations

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(
@@ -18,17 +18,32 @@ class UserRegistrationForm(forms.Form):
             attrs={"class": "form-control", "placeholder": "Enter Your Password"}
         )
     )
+    repeat_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Enter Your Password again"}
+        )
+    )
 
+    # Validate Email
     def clean_email(self):
         email = self.cleaned_data["email"]
         user = User.objects.filter(email=email).exists()
         if user:
-            raise forms.ValidationError("Email already registered")
+            raise ValidationError("Email already registered")
         return email
 
+    # Validate Username
     def clean_username(self):
         username = self.cleaned_data["username"]
         user = User.objects.filter(username=username).exists()
         if user:
-            raise forms.ValidationError("Username already registered")
+            raise ValidationError("Username already registered")
         return username
+
+    #validate password and confirm password
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        repeat_password = cleaned_data.get("repeat_password")
+        if password and repeat_password and  password != repeat_password:
+            raise ValidationError("Passwords do not match")
